39146edd8efb3c7315a5b5318417291ed998076d
Nazirjon0125 <tukhtamirzaevnazirjon96@gmail.com>
2025-04-16 19:08:43 +0900

    fix: develop Burak screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,40 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { HomePage } from "./screens/homePage";
+import { UserPage } from "./screens/userPage";
+import { OrdersPage } from "./screens/orders";
+import { ProductsPage } from "./screens/productsPage";
 
 function App() {
   return (
-    <Container sx={{ background: "orange" }}>
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h4"}>
-            Create React App on TypeScript with REDUX
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">HomePage</Link>
+          </li>
+          <li>
+            <Link to="/products">ProductsPage</Link>
+          </li>
+          <li>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">UserPage</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/products" element={<ProductsPage />}></Route>
+        <Route path="/orders" element={<OrdersPage />}></Route>
+        <Route path="/member-page" element={<UserPage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </div>
   );
 }
 
