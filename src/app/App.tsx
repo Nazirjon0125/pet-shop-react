91b2adf09b493b65160eb7d6d5f4ab5ce512af4a
Nazirjon0125 <tukhtamirzaevnazirjon96@gmail.com>
2025-04-19 01:19:48 +0900

    feat: integration footer and basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,18 @@ import React from "react";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
-import { UserPage } from "./screens/userPage";
-import { OrdersPage } from "./screens/orders";
-import { ProductsPage } from "./screens/productsPage";
-import { HomeNavbar } from "./components/header/HomeNavbar";
-import { OtherNavbar } from "./components/header/OtherNavbar";
-import { Footer } from "./components/footer";
-import { HelpPage } from "./screens/helpPage";
+import HomePage from "./screens/homePage";
+import UserPage from "./screens/userPage";
+import OrdersPage from "./screens/orders";
+import ProductsPage from "./screens/productsPage";
+import HelpPage from "./screens/helpPage";
+import HomeNavbar from "./components/header/HomeNavbar";
+import OtherNavbar from "./components/header/OtherNavbar";
+import Footer from "./components/footer";
+
 import "../css/app.css";
 import "../css/navbar.css";
+import "../css/footer.css";
 
 function App() {
   const location = useLocation();
