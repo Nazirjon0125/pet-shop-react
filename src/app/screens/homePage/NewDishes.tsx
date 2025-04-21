980829251ca2bf0ac0874031308fd7369c86dc42
Nazirjon0125 <tukhtamirzaevnazirjon96@gmail.com>
2025-04-21 16:43:44 +0900

    fix: organize homepage related sectional components

---
+++ src/app/screens/homePage/NewDishes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function NewDishes() {
+  return (
+    <div className={"homepage"}>
+      <Container>NewDishes</Container>
+    </div>
+  );
+}
---
+++ src/app/screens/homePage/Statistics.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Statistics() {
+  return (
+    <div className={"homepage"}>
+      <Container>Statistics</Container>
+    </div>
+  );
+}
