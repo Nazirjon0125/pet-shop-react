3ebbe7256b99dce383b31a0e587ba324abff707e
John <tukhtamirzaevnazirjon96@gmail.com>
2025-05-17 00:03:36 +0900

    fix: retrive menu and modify NewDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,23 +6,24 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import Events from "./Events";
 import ActiveUsers from "./ActiveUsers";
-import "../../../css/home.css";
 
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
 
 /* REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 export default function HomePage() {
   // Selector: Store => Data
-  const { setPopularDishes } = actionDispatch(useDispatch()); // reduser
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch()); // reduser
 
   useEffect(() => {
     // Backend server data request => Data
@@ -33,11 +34,24 @@ export default function HomePage() {
         page: 1,
         limit: 4,
         order: "productViews",
-        productCollection: ProductCollection.DISH,
+        // productCollection: ProductCollection.DISH,
       })
       .then((data) => {
         console.log("data passed here", data);
         setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+        productCollection: ProductCollection.DISH,
+      })
+      .then((data) => {
+        console.log("data passed here", data);
+        setNewDishes(data);
       })
       .catch((err) => console.log(err));
     //Slice: Data => Store
