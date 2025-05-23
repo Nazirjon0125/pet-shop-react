57e5765b54bdba54a04c77e8f32d4527be030220
John <tukhtamirzaevnazirjon96@gmail.com>
2025-05-24 04:26:19 +0900

    feat: create useGlobals integrated with Context & develop logout process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,16 +14,40 @@ import Test from "./screens/Test";
 import "../css/app.css";
 import "../css/navbar.css";
 import "../css/footer.css";
+import { T } from "../lib/types/common";
+import { sweetErrorHandling, sweetTopSuccessAlert } from "../lib/sweetAlert";
+import { Message } from "../lib/config";
+import MemberService from "./services/MemberService";
+import { useGlobals } from "./hooks/useGlobals";
 
 function App() {
   const location = useLocation();
+  const { setAuthMember } = useGlobals();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
   const [signupOpen, setSignupOpen] = useState<boolean>(false);
   const [loginOpen, setLoginOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   /* HANDLERS */
   const handleSignupClose = () => setSignupOpen(false);
   const handleLoginClose = () => setLoginOpen(false);
+
+  const handleLogoutClick = (e: T) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleCloseLoguot = () => setAnchorEl(null);
+  const handleLogoutRequest = async () => {
+    try {
+      const member = new MemberService();
+      await member.logout();
+
+      await sweetTopSuccessAlert("success", 700);
+      setAuthMember(null);
+    } catch (err) {
+      console.log(err);
+      sweetErrorHandling(Message.error1);
+    }
+  };
 
   return (
     <>
@@ -36,6 +60,10 @@ function App() {
           onDeleteAll={onDeleteAll}
           setSignupOpen={setSignupOpen}
           setLoginOpen={setLoginOpen}
+          anchorEl={anchorEl}
+          handleLogoutClick={handleLogoutClick}
+          handleCloseLoguot={handleCloseLoguot}
+          handleLogoutRequest={handleLogoutRequest}
         />
       ) : (
         <OtherNavbar
@@ -46,6 +74,10 @@ function App() {
           onDeleteAll={onDeleteAll}
           setSignupOpen={setSignupOpen}
           setLoginOpen={setLoginOpen}
+          anchorEl={anchorEl}
+          handleLogoutClick={handleLogoutClick}
+          handleCloseLoguot={handleCloseLoguot}
+          handleLogoutRequest={handleLogoutRequest}
         />
       )}
       <Routes>
---
+++ src/app/context/ContextProvider.tsx
@@ -0,0 +1,27 @@
+import Cookies from "universal-cookie";
+import React, { ReactNode, useState } from "react";
+import { Member } from "../../lib/types/member";
+import { GlobalContext } from "../hooks/useGlobals";
+
+const ContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const cookies = new Cookies();
+  // Cookie yo‘q bo‘lsa, localStorage ni tozalash
+  if (!cookies.get("accessToken")) localStorage.removeItem("memberData");
+
+  // Auth memberni localStorage dan olish
+  const [authMember, setAuthMember] = useState<Member | null>(
+    localStorage.getItem("memberData")
+      ? JSON.parse(localStorage.getItem("memberData") as string)
+      : null
+  );
+
+  console.log("=== verfy ===");
+
+  return (
+    <GlobalContext.Provider value={{ authMember, setAuthMember }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default ContextProvider;
